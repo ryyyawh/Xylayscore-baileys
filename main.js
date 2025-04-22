0de5ead40829b03722e6d4c124ecfb1b31dae23c
Xylays <aulidanraja@gmail.com>
2025-04-22 14:53:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const log = require('./lib/log');
 log.info('Bot Starting...');
 log.success('Koneksi berhasil! Xylays In Here');
 log.warn('Waspada: kemungkinan spam!');
-log.error('Gagal connect ke WhatsApp!');
 
 (async () => {
   const sock = await connectToWhatsApp(token);
